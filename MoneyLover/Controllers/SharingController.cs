6912243a4961e685c09d5a06a33264d7b859e2c4
Hieu Tran <hieutran106@gmail.com>
2018-04-20 17:21:15 +1000

    Before add customer model binder.

--- MoneyLover/Controllers/SharingController.cs
+++ MoneyLover/Controllers/SharingController.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MoneyLover.Models;
+using MoneyLover.Models.ViewModels;
 
 namespace MoneyLover.Controllers
 {
     public class SharingController : Controller
     {
+        private IRepository repo;
+        private UserManager<AppUser> userManager;
+        public SharingController(IRepository repo, UserManager<AppUser> userManager)
+        {
+            this.repo = repo;
+            this.userManager = userManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<AppUser> allUsers = new List<AppUser>();
+            foreach (AppUser user in userManager.Users)
+            {
+                allUsers.Add(user);
+            }
+            SharingSelectionModel model = new SharingSelectionModel
+            {
+                Ids= new string[allUsers.Count()],
+                Usernames = new string[allUsers.Count()],
+                ToDate = DateTime.Today,
+                FromDate = DateTime.Today.AddDays(-14)
+            };
+            for (int i=0;i<allUsers.Count();i++)
+            {
+                model.Ids[i] = allUsers[i].Id;
+                model.Usernames[i] = allUsers[i].UserName;
+            }
+            
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Sharing(SharingSelectionModel model)
+        {
+
+            SharingModel a = new SharingModel();
+            return View(a);
         }
     }
 }
---
+++ MoneyLover/Models/ViewModels/SharingSelectionModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoneyLover.Models.ViewModels
+{
+    public class SharingSelectionModel
+    {
+        public string[] Ids { get; set; }
+        public string[] Usernames { get; set; }
+        public IEnumerable<AppUser> Stakeholders { get; set; }
+
+        [Required]      
+        public DateTime FromDate { get; set; }
+
+        [Required]
+        public DateTime ToDate { get; set; }
+
+    }
+ 
+    
+    public class SharingModel
+    {
+        public IEnumerable<AppUser> Users;
+        public decimal Total;
+        public decimal Average;
+    }
+}
